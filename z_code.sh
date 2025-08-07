d66d66e1c0eb6fd2cb3af244331e48f4d2966765
Aricling <aricling69@gmail.com>
2025-08-07 11:23:18 +0800

    add finetune clip code

--- z_code.sh
+++ z_code.sh
@@ -1,11 +1,22 @@
+## 其实是finetune clip训练的启动代码
+python -m train.train_mdm_w_motionbert \
+    --save_dir save/finetune_clip_all_ffn_layers \
+    --dataset humanml \
+    --diffusion_steps 50 \
+    --arch trans_dec \
+    --text_encoder_type clip \
+    --mask_frames \
+    --use_ema \
+    --device 0 \
+    --overwrite \
+    --train_platform_type WandBPlatform \
+    --pooling 1 \
+    --motionbert_config /home/mengqing/usr/motion-diffusion-model/MotionBERT/configs/pose3d/MB_ft_h36m_global_lite.yaml \
+    --evaluate_motionbert /home/mengqing/usr/motion-diffusion-model/MotionBERT/checkpoint/pose3d/FT_MB_lite_MB_ft_h36m_global_lite/best_epoch.bin
+    # --multiview
+
 # eval fintuned CLIP启动代码
 python -m eval.eval_humanml --model_path /home/mengqing/usr/motion-diffusion-model/save/finetune_clip_all__layers/lora000600000.pt
-
-
-
-
-
-
 
 
 # 训来MDM+distilled Bert的代码
